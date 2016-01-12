24a6f9d60d6e7c78d13a8188fe72b3639c0e8321
Brian Donohue <brian.donohue89@gmail.com>
2016-01-11 22:57:44 -0500

    Remove extra logging
    Signed-off-by: Brian Donohue <brian.donohue89@gmail.com>

--- main.py
+++ main.py
@@ -90,10 +90,8 @@ def oauth():
         'client_secret': SLACK_CLIENT_SECRET,
         'code': code
     }
-    print params
     try:
         response = requests.post('https://slack.com/api/oauth.access', data=params)
-        print response.text
     except Exception, e:
         return 'Slack request failed: %s' % str(e)
     return 'Success'
